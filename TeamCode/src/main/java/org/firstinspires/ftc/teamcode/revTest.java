1c719d245e2e584caa73fc591c0a542d5271fac2
papadxpoulos <brxno633@gmail.com>
2026-02-18 21:38:04 +0200

    made it work properly

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/revTest.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class revTest extends OpMode {
+    public void init() {
+
+    }
+    public voidd loop() {
+
+    }
+
+
+}
