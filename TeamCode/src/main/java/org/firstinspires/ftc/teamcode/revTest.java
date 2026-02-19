eb5f24d834d26e12534a64de516ad0d689d83b6f
papadxpoulos <brxno633@gmail.com>
2026-02-19 12:53:46 +0200

    dist1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/revTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/revTest.java
@@ -2,13 +2,15 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 @TeleOp
 public class revTest extends OpMode {
     public void init() {
 
     }
-    public voidd loop() {
+    public void loop() {
 
     }
 
