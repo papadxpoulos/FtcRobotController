cdece3acd73115be3bc5adddd5ae099cc4daf12d
papadxpoulos <brxno633@gmail.com>
2026-02-20 14:13:02 +0200

    hw map

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/revTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/revTest.java
@@ -9,7 +9,8 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 public class revTest extends OpMode {
     @Override
     public void init() {
-
+        sensorDistance1 = hardwareMap.get(DistanceSensor.class, "sensor_distance"); // change sensor_distance1 to its name
+        sensorDistance2 = hardwareMap.get(DistanceSensor.class, "sensor_distance"); // change sensor_distance2 to its name
     }
     @Override
     public void loop() {
