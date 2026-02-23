8d289870b51b20e41aafc1b935812d1aea82658d
papadxpoulos <brxno633@gmail.com>
2026-02-23 17:17:58 +0200

    distance sensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/revTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/revTest.java
@@ -1,21 +1,24 @@
 package org.firstinspires.ftc.teamcode;
 
+import org.firstinspires.ftc.teamcode.me
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
 @TeleOp
 public class revTest extends OpMode {
+
+    mechDistance dist = new mechDistance();
+
     @Override
     public void init() {
-        sensorDistance1 = hardwareMap.get(DistanceSensor.class, "distance sensor 1");
-        sensorDistance2 = hardwareMap.get(DistanceSensor.class, "distance sensor 2");
+        dist.init(hardwareMap);
     }
     @Override
     public void loop() {
-
+        telemetry.addData("distance", dist.getDistance());
     }
-
 
 }
