210d65222b8495939479803c9e5585fc9278eb2a
papadxpoulos <brxno633@gmail.com>
2026-02-23 16:45:43 +0200

    mech distance 1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechDistance.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechDistance.java
@@ -1,4 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 public class mechDistance {
+    private DistanceSensor distance;
+    
+    public void
 }
