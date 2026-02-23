b6fba5d2a1a76aa36ca289f4bdba49e5d5d9b533
papadxpoulos <brxno633@gmail.com>
2026-02-23 17:21:19 +0200

    mechDistance import

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/revTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/revTest.java
@@ -1,6 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
-import org.firstinspires.ftc.teamcode.me
+import org.firstinspires.ftc.teamcode.mechDistance;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
