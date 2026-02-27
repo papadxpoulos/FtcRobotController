be636d2b7a7f741eef755d95ef1f2c840e8385a3
papadxpoulos <brxno633@gmail.com>
2026-02-27 08:20:33 +0200

    servo hw map

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechServo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechServo.java
@@ -1,4 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 public class mechServo {
+    private Servo servoPos;
+
+    public void init(HardwareMap hwMap) {
+        servoPos = hwMap.get(Servo.class,"port0exp");
+        
+    }
 }
